27d821b88aa05ee9d539b5d55eca6637cd405a05
Merlyn Albery-Speyer <merlyn@newrelic.com>
2020-08-08 14:57:26 -0700

    First cut.

---
+++ what-not-in-git.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'pathname'
+
+puts ARGV.inspect
+unless ARGV.length >= 1 && ARGV.all? { |arg| File.exists?(arg) && File.directory?(arg) }
+    STDERR.puts "Usage:"
+    STDERR.puts "what-not-in-git.rb PATH_TO_PROJECTS [PATH_TO_PROJECTS ...]"
+    exit 0
+end
+
+ARGV.map do |arg|
+    if File.exists?(Pathname.new(arg) + ".git")
+        arg
+    else
+        Dir.glob(Pathname.new(arg)+"*")
+    end
+end.flatten.each do |dir|
+    next unless File.directory?(dir)
+    
+    print dir
+
+    if File.exists?(Pathname.new(dir) + ".git")
+        Dir.chdir(dir)
+        git_status = `git status --porcelain --branch`
+        if git_status.match(/^A/)
+            puts " ❌ changes to be committed"
+        elsif git_status.match(/^ M/)
+            puts " ❌ modified files"
+        elsif git_status.match(/^ D/)
+            puts " ❌ deleted files"
+        elsif git_status.include?("??")
+            puts " ❌ untracked files"
+        elsif git_status.match(/\A## master\.\.\..*\n\Z/)
+            puts " clean ✅"
+        else
+            puts git_status.inspect
+            exit(0)
+        end
+    else
+        puts " ❌ not tracked in git"
+    end
+end
